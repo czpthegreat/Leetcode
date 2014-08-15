95f1be84bab96c4fbe3df0ba3068bf29e4364836
ZP.Chen <czpthegreat@gmail.com>
2014-08-16 01:00:28 +0800

    Remove Duplicates From Sorted Array

---
+++ java/RemoveDuplicatesFromSortedArray1.java
@@ -0,0 +1,36 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] A = {1,1,2};
+		System.out.println(Solution.removeDuplicates(A));
+		
+		int[] A2 = {1,1,1,2,2,2,3};
+		System.out.println(Solution.removeDuplicates(A2));
+	}
+
+	public static class Solution {
+		public static int removeDuplicates(int[] A) {
+			int lastValue = 0;
+			int length = 0;
+
+			if (A.length == 0) {
+				return 0;
+			}
+			for (int i = 0; i < A.length; i++) {
+				if (i == 0) {
+					lastValue = A[0];
+					length++;
+				} else {
+					if (A[i] != lastValue) {
+						A[length] = A[i];
+						length++;
+						lastValue = A[i];
+					}
+				}
+			}
+			
+			return length;
+		}
+	}
+}
